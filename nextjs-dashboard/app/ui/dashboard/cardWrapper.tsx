e304e5ac041868283a8d920dd3c61edb74470252
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-06-19 16:51:50 +0300

    feat: add some functions

---
+++ nextjs-dashboard/app/ui/dashboard/cardWrapper.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Card } from './cards'
+import { fetchCardData } from '@/app/lib/data';
+
+export default async function CardWrapper() {
+    const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData();
+  return (
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+    <Card title="Collected" value={totalPaidInvoices} type="collected" />
+    <Card title="Pending" value={totalPendingInvoices} type="pending" />
+    <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+    <Card
+      title="Total Customers"
+      value={numberOfCustomers}
+      type="customers"
+    />
+  </div>
+  )
+}
